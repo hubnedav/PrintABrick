c9cc467b846299e9f8b8c6b012f0520305561bb5
David Hübner <hubnedav@fit.cvut.cz>
2016-12-02 00:21:00 +0100

    Setup gulp, semantic

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')(),
+    build_sematnic = require('./app/Resources/assets/semantic/tasks/build'),
+    watch_sematnic = require('./app/Resources/assets/semantic/tasks/watch');
+
+
+gulp.task('semantic:build', build_sematnic);
+
+gulp.task('semantic:watch', watch_sematnic);
+
+gulp.task('css', function() {
+    return gulp.src([
+        'app/Resources/assets/semantic/dist/semantic.css',
+    ])
+        .pipe(plugins.sass().on('error', plugins.sass.logError))
+        .pipe(plugins.concat('main.css', {newLine: ' '}))
+        .pipe(gulp.dest('web/resources/css'));
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.js',
+        'app/Resources/assets/semantic/dist/semantic.js',
+    ])
+        .pipe(plugins.concat('main.js'))
+        .pipe(gulp.dest('web/resources/js'));
+});
+
+gulp.task('default', ['semantic:build'], function () {
+    return gulp.start(['js', 'css']);
+});

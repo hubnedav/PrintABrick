0f7346068dbcdf7716f20f4c3d1bb4aca868bcf3
David Hübner <hubnedav@fit.cvut.cz>
2017-03-10 23:37:23 +0100

    Add RebrickableLoader
    Loader to obtain Rebrickable database dump and load it to local database

--- src/AppBundle/Command/LoadRebrickableDataCommand.php
+++ src/AppBundle/Command/LoadRebrickableDataCommand.php
@@ -3,32 +3,27 @@
 namespace AppBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class LoadLibraryCommand extends ContainerAwareCommand
+class LoadRebrickableDataCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
         $this
-            ->setName('app:load:library')
-            ->setDescription('Loads LDraw library parts')
-            ->setHelp('This command allows you to..')
-            ->addArgument('ldraw', InputArgument::OPTIONAL, 'Path to LDraw library folder');
+            ->setName('app:load:rebrickable')
+            ->setDescription('Loads Rebrickable database')
+            ->setHelp('This command allows you to..');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $ldrawLoader = $this->getContainer()->get('loader.ldraw');
-        $ldrawLoader->setOutput($output);
-
         $rebrickableLoader = $this->getContainer()->get('loader.rebrickable');
         $rebrickableLoader->setOutput($output);
 
-        try {
-            $ldrawLoader->loadModels($input->getArgument('ldraw'));
+        //TODO log errors
 
+        try {
             $rebrickableLoader->loadColors();
 
             $rebrickableLoader->loadParts();

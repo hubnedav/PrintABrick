7c747b0b9d737cea441d28f81cadbc0870b13218
Unknown <davhubner@gmail.com>
2017-06-02 02:58:59 +0200

    Update Symfony to 3.3

--- web/app.php
+++ web/app.php
@@ -7,7 +7,7 @@ $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
+//$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
--- web/app_dev.php
+++ web/app_dev.php
@@ -23,7 +23,7 @@ $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+//$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();

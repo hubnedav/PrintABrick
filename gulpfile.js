d6513c28d7d4335cbaa65ff33b298a417e5a53f6
Unknown <davhubner@gmail.com>
2017-06-03 23:07:55 +0200

    Load assets with bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ var gulp = require('gulp'),
 
 gulp.task('css', function() {
     return gulp.src([
-        'node_modules/semantic-ui/dist/semantic.css',
-        'node_modules/lightbox2/dist/css/lightbox.css',
+        'bower_components/semantic/dist/semantic.css',
+        'bower_components/lightbox2/dist/css/lightbox.css',
         'bower_components/jquery-ui/themes/base/jquery-ui.css',
         'bower_components/jQuery-ui-Slider-Pips/dist/jquery-ui-slider-pips.css',
         'bower_components/select2/dist/css/select2.css',
         'app/Resources/assets/style/style.scss',
     ])
         .pipe(plugins.sass().on('error', plugins.sass.logError))
-        .pipe(plugins.concat('main.css', {newLine: ' '}))
+        .pipe(plugins.concat('style.css', {newLine: ' '}))
         .pipe(plugins.environments.production(plugins.cssmin()))
         .pipe(gulp.dest('web/resources/css'));
 });
@@ -43,25 +43,25 @@ gulp.task('js', function() {
         'bower_components/jqueryui-touch-punch/jquery.ui.touch-punch.js',
         'bower_components/select2/dist/js/select2.full.js',
         'bower_components/jquery-address/src/jquery.address.js',
-        'node_modules/semantic-ui/dist/semantic.js',
-        'node_modules/lightbox2/dist/js/lightbox.js',
+        'bower_components/semantic/dist/semantic.js',
+        'bower_components/lightbox2/dist/js/lightbox.js',
         'app/Resources/assets/js/**.js',
     ])
-        .pipe(plugins.concat('main.js'))
+        .pipe(plugins.concat('style.js'))
         .pipe(plugins.environments.production(plugins.uglify()))
         .pipe(gulp.dest('web/resources/js'));
 });
 
 gulp.task('files:semantic', function () {
     return gulp.src(
-        'node_modules/semantic-ui/dist/themes/**'
+        'bower_components/semantic/dist/themes/**'
     )
-        .pipe(gulp.dest('web/resources/css/themes'));
+        .pipe(gulp.dest('web/resources/css/semantic-ui'));
 });
 
 gulp.task('files:images', function () {
     return gulp.src([
-        'node_modules/lightbox2/dist/images/**',
+        'bower_components/lightbox2/dist/images/**',
         'app/Resources/assets/images/**'
     ])
         .pipe(gulp.dest('web/resources/images'));
